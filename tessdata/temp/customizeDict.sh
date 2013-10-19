3dc5eeb4231864ef75453fe896b9eb0c3cf026c1
Kevin.Li <li_zhaoyu@msn.com>
2013-10-19 12:13:51 +0800

    准备自动生成词典

---
+++ tessdata/temp/customizeDict.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp -f ../chi_sim.traineddata_bk chi_sim.traineddata
+rm -f .*
+combine_tessdata -u chi_sim.traineddata
+wordlist2dawg ../chi_sim.user-words chi_sim.word-dawg .unicharset
+combine_tessdata -o chi_sim.traineddata chi_sim.word-dawg
+#cp -f chi_sim.traineddata ../
